8782db029a5702ca9094bbde6efe964e1a32fcc1
Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
2023-09-25 22:09:16 +0200

    refactor(Client): clean Background display code by using classes, etc...

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,31 +7,12 @@
 
 #include <SFML/Graphics.hpp>
 
-#include "../build/assets/Space_Background.hpp"
+#include "client/Game/Game.hpp"
 
 int main(void)
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "R-Type");
-    sf::Texture texture;
-    texture.loadFromMemory(___assets_Space_Background_png, ___assets_Space_Background_png_len);
-    sf::Sprite sprite(texture);
-    sf::IntRect desiredRect(0, 0, 300, 207);
-    float scaleX = static_cast<float>(window.getSize().x) / desiredRect.width;
-    float scaleY = static_cast<float>(window.getSize().y) / desiredRect.height;
+    Game game;
 
-    sprite.setScale(scaleX, scaleY);
-    sprite.setPosition(0, 0);
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        window.draw(sprite);
-        window.display();
-    }
+    game.run();
     return 0;
 }
