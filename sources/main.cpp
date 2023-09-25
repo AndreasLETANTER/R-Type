c6dfcb94ccf32aca1362980ddaa0f01fc7ffc076
Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
2023-09-25 18:33:34 +0200

    feat(Client): add background to SFML window

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,16 +1,17 @@
-/*
-** EPITECH PROJECT, 2023
-** MIRROR_R-Type
-** File description:
-** main
-*/
-
 #include <SFML/Graphics.hpp>
 
 int main(void)
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "R-Type");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "R-Type");
+    sf::Texture texture;
+    texture.loadFromFile("assets/Space_Background.png");
+    sf::Sprite sprite(texture);
+    sf::IntRect desiredRect(0, 0, 300, 207);
+    float scaleX = static_cast<float>(window.getSize().x) / desiredRect.width;
+    float scaleY = static_cast<float>(window.getSize().y) / desiredRect.height;
 
+    sprite.setScale(scaleX, scaleY);
+    sprite.setPosition(0, 0);
     while (window.isOpen())
     {
         sf::Event event;
@@ -19,7 +20,8 @@ int main(void)
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear(sf::Color::Black);
+        window.clear();
+        window.draw(sprite);
         window.display();
     }
     return 0;
