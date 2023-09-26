145afab3c4b72f7dbb5969f434b79fd18b1db1af
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-09-26 10:13:53 +0200

    refactor: New map for storing key value in control system

--- src/Systems/ControlSystem/ControlSystem.cpp
+++ src/Systems/ControlSystem/ControlSystem.cpp
@@ -6,28 +6,29 @@
 */
 
 #include "ControlSystem.hpp"
-#include <SFML/Graphics.hpp>
+#include <map>
+#include <SFML/Window/Keyboard.hpp>
 
 ControlSystem ControlSystem::operator()(Registry &registry, SparseArray<Component::Controllable> &controllables, SparseArray<Component::Velocity> &velocities)
 {
     (void)registry;
+    std::map<sf::Keyboard::Key, std::pair<int, int>> KeyMap;
+
+    KeyMap[sf::Keyboard::Z] = std::make_pair(0, -5);
+    KeyMap[sf::Keyboard::S] = std::make_pair(0, 5);
+    KeyMap[sf::Keyboard::Q] = std::make_pair(-5, 0);
+    KeyMap[sf::Keyboard::D] = std::make_pair(5, 0);
     for (size_t i = 0; i < controllables.size() && i < velocities.size(); ++i) {
         auto &con = controllables[i];
         auto &vel = velocities[i];
         if (con.has_value() && con.value().status && vel.has_value()) {
             vel.value().vx = 0;
             vel.value().vy = 0;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
-                vel.value().vy = -5;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-                vel.value().vy = 5;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-                vel.value().vx = -5;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-                vel.value().vx = 5;
+            for (auto &key : KeyMap) {
+                if (sf::Keyboard::isKeyPressed(key.first)) {
+                    vel.value().vx += key.second.first;
+                    vel.value().vy += key.second.second;
+                }
             }
         }
     }
