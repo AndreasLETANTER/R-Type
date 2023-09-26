5c8a5413dfe763b1895a13f89f523b7979204888
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-09-26 12:06:59 +0200

    refactor: Drawable is now back to class from struct

--- sources/ECS/Components/Drawable.hpp
+++ sources/ECS/Components/Drawable.hpp
@@ -13,15 +13,16 @@ namespace Component {
      * @brief This component represents an entity's drawable object.
      * 
      */
-    typedef struct Drawable {
-        sf::Sprite sprite; /**< The sprite of the drawable object. */
-        sf::RenderWindow *window; /**< The render window to draw the object on. */
-        /**
-         * @brief Construct a new Drawable object.
-         * 
-         * @param sprite The sprite of the drawable object.
-         * @param window The render window to draw the object on.
-         */
-        inline Drawable(sf::Sprite sprite, sf::RenderWindow *window) { this->sprite = sprite; this->window = window; }
+    typedef class Drawable {
+        public:
+            sf::Sprite sprite; /**< The sprite of the drawable object. */
+            sf::RenderWindow *window; /**< The render window to draw the object on. */
+            /**
+             * @brief Construct a new Drawable object.
+             * 
+             * @param sprite The sprite of the drawable object.
+             * @param window The render window to draw the object on.
+             */
+            inline Drawable(sf::Sprite sprite, sf::RenderWindow *window) { this->sprite = sprite; this->window = window; }
     } Drawable;
 }
