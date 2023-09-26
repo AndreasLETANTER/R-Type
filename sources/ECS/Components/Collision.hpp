4da4d42ec4bbe7a9bdd0b864913020bc8b83e349
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-09-26 16:44:51 +0200

    docs: collision system and collision component
    documentation for both

--- sources/ECS/Components/Collision.hpp
+++ sources/ECS/Components/Collision.hpp
@@ -12,11 +12,20 @@
 #include "../EntityClass/Entity.hpp"
 
 namespace Component {
+    /**
+     * @brief Component that represents a collision box for an entity.
+     */
     class Collision {
         public:
-            std::vector<Entity> entities_in_collision;
-            int height;
-            int width;
+            std::vector<Entity> entities_in_collision; /**< List of entities that are currently colliding with this collision box. */
+            int height; /**< Height of the collision box. */
+            int width; /**< Width of the collision box. */
+            /**
+             * @brief Construct a new Collision object with the given height and width.
+             * 
+             * @param height Height of the collision box.
+             * @param width Width of the collision box.
+             */
             inline Collision(int height, int width) { this->height = height; this->width = width; }
     };
 }
--- sources/ECS/Systems/CollisionSystem/CollisionSystem.hpp
+++ sources/ECS/Systems/CollisionSystem/CollisionSystem.hpp
@@ -11,9 +11,13 @@
 #include "../../Components/Position.hpp"
 #include "../../Components/Collision.hpp"
 
+/**
+ * @brief The CollisionSystem class handles the collision detection and resolution for entities with a Position and Collision component.
+ */
 class CollisionSystem {
     public:
         CollisionSystem() = default;
         ~CollisionSystem() = default;
+
         CollisionSystem operator()(Registry &registry, SparseArray<Component::Position> &positions, SparseArray<Component::Collision> &collisions);
 };
