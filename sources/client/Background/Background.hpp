4929e7cf960cc92bb9bd82f59966ec5df3ea7af2
Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
2023-09-26 09:38:43 +0200

    docs(Client): add doc to Game and Background files

--- sources/client/Background/Background.hpp
+++ sources/client/Background/Background.hpp
@@ -11,9 +11,31 @@
 
 class Background {
     public:
+        /**
+         * @brief The Background class represents the background of the game.
+         */
         Background() = default;
+
+        /**
+         * @brief Loads a texture from the given data.
+         *
+         * @param data Pointer to the data containing the texture.
+         * @param size Size of the data in bytes.
+         */
         void loadTexture(const unsigned char* data, std::size_t size);
+
+        /**
+         * @brief Sets the size of the background to match the size of the window.
+         *
+         * @param windowSize The size of the window as a 2D vector of unsigned integers.
+         */
         void setSize(sf::Vector2u windowSize);
+
+        /**
+         * @brief Draws the background on the given window.
+         *
+         * @param window The window to draw the background on.
+         */
         void draw(sf::RenderWindow& window);
     private:
         sf::Texture m_texture;
--- sources/client/Game/Game.hpp
+++ sources/client/Game/Game.hpp
@@ -13,11 +13,30 @@
 
 class Game {
     public:
+        /**
+         * @brief Constructs a new Game object.
+         */
         Game();
+
+        /**
+         * @brief Runs the game loop.
+         */
         void run();
     private:
+        /**
+         * @brief Processes all events in the event queue.
+         */
         void processEvents();
+
+        /**
+         * @brief Renders the game state to the screen.
+         *
+         * This method is responsible for rendering the current game state to the screen.
+         * It should be called once per frame to ensure that the screen is updated with the
+         * latest game state.
+         */
         void render();
+
         sf::RenderWindow m_window;
         Background m_background;
 };
