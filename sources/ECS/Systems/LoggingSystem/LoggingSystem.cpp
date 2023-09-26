68bd5bba48f3aa722b1ad85bcc8216d9447a9306
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-09-26 12:22:36 +0200

    style: space between two lines

--- sources/ECS/Systems/LoggingSystem/LoggingSystem.cpp
+++ sources/ECS/Systems/LoggingSystem/LoggingSystem.cpp
@@ -13,6 +13,7 @@ LoggingSystem LoggingSystem::operator()(Registry &registry, SparseArray<Componen
     for (size_t i = 0; i < positions.size() && i < velocities.size(); ++i) {
         auto const &pos = positions[i];
         auto const &vel = velocities[i];
+
         if (pos.has_value() && vel.has_value()) {
             std::cerr << i << ": Position = { " << pos.value().x << ", " << pos.value().y << " } , Velocity = { " << vel.value().vx << ", " << vel.value().vy << " }" << std::endl;
         }
