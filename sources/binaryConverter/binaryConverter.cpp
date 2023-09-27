c5b9c83d8c1aad1836ddd4b470dfa6faf4bb6bf2
Luchooooo <louis.barreteau@epitech.eu>
2023-09-27 13:36:18 +0200

    feat: error class in binary converter

--- sources/binaryConverter/binaryConverter.cpp
+++ sources/binaryConverter/binaryConverter.cpp
@@ -8,6 +8,7 @@
 #include <bitset>
 #include <stdexcept>
 
+#include "../debugColors.hpp"
 #include "binaryConverter.hpp"
 
 std::string binaryConverter::string_to_binary(std::string t_str)
@@ -25,12 +26,16 @@ std::string binaryConverter::binary_to_string(std::string t_str)
     std::string str;
     std::string byte;
 
-    for (size_t i = 0; i < t_str.length(); i += 8) {
-        byte = t_str.substr(i, 8);
-        if (byte.find_first_not_of("01") != std::string::npos) {
-            throw std::invalid_argument("Invalid binary string");
+    try {
+        for (size_t i = 0; i < t_str.length(); i += 8) {
+            byte = t_str.substr(i, 8);
+            if (byte.find_first_not_of("01") != std::string::npos) {
+                throw std::invalid_argument("Invalid binary string");
+            }
+            str += std::bitset<8>(byte).to_ulong();
         }
-        str += std::bitset<8>(byte).to_ulong();
+    } catch (std::invalid_argument const &e) {
+        printError(e.what());
     }
     return str;
 }
