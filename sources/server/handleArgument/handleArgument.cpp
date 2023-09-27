a93e061f6892f4c5c813a168a7f3ec5a03dca6a7
Eliot Baratte <eliot.baratte@epitech.eu>
2023-09-27 17:44:46 +0200

    fix(handleArgument): remove unnecessary method that throws a Werror

--- sources/server/handleArgument/handleArgument.cpp
+++ sources/server/handleArgument/handleArgument.cpp
@@ -13,7 +13,6 @@ u_int16_t handleArgument::getPort(std::string const &t_av) const
 
     try {
         check_if_number(t_av);
-        check_if_is_in_range(std::stoi(t_av));
         port = std::stoi(t_av);
     } catch (std::invalid_argument const &e) {
         std::cerr << e.what() << std::endl;
@@ -31,12 +30,4 @@ void handleArgument::check_if_number(std::string const &t_str) const
             throw std::invalid_argument("Invalid argument: not a number or not a positive number");
         }
     }
-}
-
-void handleArgument::check_if_is_in_range(u_int16_t const &port) const
-{
-    (void)port;
-    // if (port < MIN_PORT || port > MAX_PORT) {
-    //     throw std::invalid_argument("Invalid argument: port is not in range");
-    // }
 }
