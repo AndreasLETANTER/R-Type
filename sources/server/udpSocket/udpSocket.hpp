a91ada80bcba07658c8f7936d1fa0413f30a814d
Luchooooo <louis.barreteau@epitech.eu>
2023-09-28 12:41:49 +0200

    feat: transform own udp library to asio boost library

--- sources/server/udpSocket/udpSocket.hpp
+++ sources/server/udpSocket/udpSocket.hpp
@@ -1,61 +1,45 @@
 /*
 ** EPITECH PROJECT, 2023
-** Upd class
+** Udp socket class
 ** File description:
-** This class init, bind and function to receive, send
+** This class send, receive and create the udp connexion
 */
 
-#include <netinet/in.h>
-#include <string>
+#pragma once
 
-#define MAXLINE 1024
+#include <string>
+#include <boost/asio.hpp>
 
 /**
- * @brief The udpSocket class provides an interface to create and manage a udpSocket socket.
+ * @brief The udpSocket class represents a UDP socket that can send and receive messages.
  */
 class udpSocket {
     public:
         /**
-         * @brief Constructs a udpSocket object with the specified port number.
+         * @brief Construct a new udpSocket object with the specified port number.
          * @param t_port The port number to bind the socket to.
          */
         udpSocket(u_int16_t t_port);
 
         /**
-         * @brief Destroys the udpSocket object and closes the socket.
+         * @brief Destroy the udpSocket object.
          */
         ~udpSocket();
 
         /**
-         * @brief Initializes the socket and binds it to the specified port.
-         */
-        void init(void);
-
-        /**
-         * @brief Receives a message from the socket.
+         * @brief Receive a message from the socket.
          * @return The received message as a string.
          */
-        std::string receive(void);
+        std::string receive();
 
         /**
-         * @brief Sends a message through the socket.
+         * @brief Send a message through the socket.
+         * 
          * @param t_msg The message to send as a string.
          */
         void send(std::string t_msg);
 
-    protected:
-        /**
-         * @brief Initializes the socket.
-         */
-        void init_socket(void);
-
-        /**
-         * @brief Binds the socket to the specified port.
-         */
-        void bind_socket(void);
-
     private:
-        int m_sockfd; /**< The socket file descriptor. */
-        struct sockaddr_in m_servAddr; /**< The server address. */
-        struct sockaddr_in m_cliAddr; /**< The client address. */
+        boost::asio::io_service m_ioService; ///< The Boost.Asio I/O service object.
+        boost::asio::ip::udp::socket m_socket; ///< The Boost.Asio UDP socket object.
 };
