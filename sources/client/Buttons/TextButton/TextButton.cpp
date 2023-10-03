19714f8c31188f3475f4563f5313c3dc9667de12
Eliot Baratte <eliot.baratte@epitech.eu>
2023-10-03 09:13:10 +0200

    perf(TextButton): remove unused variables

--- sources/client/Buttons/TextButton/TextButton.cpp
+++ sources/client/Buttons/TextButton/TextButton.cpp
@@ -6,80 +6,89 @@
 */
 
 #include "client/Buttons/TextButton/TextButton.hpp"
+#include "TextButton.hpp"
 
-TextButton TextButton::setButtonPosition(const sf::Vector2f &position)
+TextButton &TextButton::setButtonPosition(const sf::Vector2f &position)
 {
     m_buttonPosition = position;
+    m_shape.setPosition(position);
     return (*this);
 }
 
-TextButton TextButton::setButtonSize(const sf::Vector2f &size)
+TextButton &TextButton::setButtonSize(const sf::Vector2f &size)
 {
     m_buttonSize = size;
+    m_shape.setSize(size);
     return (*this);
 }
 
-TextButton TextButton::setButtonColor(const sf::Color &color)
+TextButton &TextButton::setButtonColor(const sf::Color &color)
 {
     m_buttonColor = color;
     return (*this);
 }
 
-TextButton TextButton::setButtonOutlineColor(const sf::Color &outlineColor)
+TextButton &TextButton::setButtonOutlineColor(const sf::Color &outlineColor)
 {
     m_buttonOutlineColor = outlineColor;
     return (*this);
 }
 
-TextButton TextButton::setButtonOutlineThickness(const int &outlineThickness)
+TextButton &TextButton::setButtonOutlineThickness(const int &outlineThickness)
 {
-    m_buttonOutlineThickness = outlineThickness;
+    m_shape.setOutlineThickness(outlineThickness);
     return (*this);
 }
 
-TextButton TextButton::setButtonHoverColor(const sf::Color &hoverColor)
+TextButton &TextButton::setButtonHoverColor(const sf::Color &hoverColor)
 {
     m_buttonHoverColor = hoverColor;
     return (*this);
 }
 
-TextButton TextButton::setButtonHoverOutlineColor(const sf::Color &hoverOutlineColor)
+TextButton &TextButton::setButtonHoverOutlineColor(const sf::Color &hoverOutlineColor)
 {
     m_buttonHoverOutlineColor = hoverOutlineColor;
     return (*this);
 }
 
-TextButton TextButton::setTextString(const std::string &text)
+TextButton &TextButton::setTextString(const std::string &text)
 {
-    m_textString = text;
+    m_text.setString(text);
     return (*this);
 }
 
-TextButton TextButton::setTextFont(const sf::Font &font)
+TextButton &TextButton::setTextFont(const sf::Font &font)
 {
-    m_textFont = font;
+    m_text.setFont(font);
     return (*this);
 }
 
-TextButton TextButton::setTextSize(const int &size)
+TextButton &TextButton::setTextSize(const int &size)
 {
-    m_textSize = size;
+    m_text.setCharacterSize(size);
     return (*this);
 }
 
-TextButton TextButton::setTextColor(const sf::Color &color)
+TextButton &TextButton::setTextPosition(const sf::Vector2f &position)
+{
+    m_text.setPosition(position);
+    return (*this);
+}
+
+TextButton &TextButton::setTextColor(const sf::Color &color)
 {
     m_textColor = color;
     return (*this);
 }
 
-TextButton TextButton::setTextHoverColor(const sf::Color &hoverColor)
+TextButton &TextButton::setTextHoverColor(const sf::Color &hoverColor)
 {
     m_textHoverColor = hoverColor;
     return (*this);
 }
 
-TextButton TextButton::setCallback(void (*callback)())
+TextButton &TextButton::setCallback(std::function<void()> callback)
 {
     m_callback = callback;
     return (*this);
--- sources/client/Buttons/TextButton/TextButton.hpp
+++ sources/client/Buttons/TextButton/TextButton.hpp
@@ -7,27 +7,29 @@
 
 #pragma once
 
+#include <functional>
 #include <SFML/Graphics.hpp>
 
 class TextButton {
     public:
         TextButton() = default;
 
-        TextButton setButtonPosition(const sf::Vector2f &position);
-        TextButton setButtonSize(const sf::Vector2f &size);
-        TextButton setButtonColor(const sf::Color &color);
-        TextButton setButtonOutlineColor(const sf::Color &outlineColor);
-        TextButton setButtonOutlineThickness(const int &outlineThickness);
-        TextButton setButtonHoverColor(const sf::Color &hoverColor);
-        TextButton setButtonHoverOutlineColor(const sf::Color &hoverOutlineColor);
+        TextButton &setButtonPosition(const sf::Vector2f &position);
+        TextButton &setButtonSize(const sf::Vector2f &size);
+        TextButton &setButtonColor(const sf::Color &color);
+        TextButton &setButtonOutlineColor(const sf::Color &outlineColor);
+        TextButton &setButtonOutlineThickness(const int &outlineThickness);
+        TextButton &setButtonHoverColor(const sf::Color &hoverColor);
+        TextButton &setButtonHoverOutlineColor(const sf::Color &hoverOutlineColor);
 
-        TextButton setTextString(const std::string &text);
-        TextButton setTextFont(const sf::Font &font);
-        TextButton setTextSize(const int &size);
-        TextButton setTextColor(const sf::Color &color);
-        TextButton setTextHoverColor(const sf::Color &hoverColor);
+        TextButton &setTextString(const std::string &text);
+        TextButton &setTextFont(const sf::Font &font);
+        TextButton &setTextSize(const int &size);
+        TextButton &setTextPosition(const sf::Vector2f &position);
+        TextButton &setTextColor(const sf::Color &color);
+        TextButton &setTextHoverColor(const sf::Color &hoverColor);
 
-        TextButton setCallback(void (*callback)());
+        TextButton &setCallback(std::function<void()> callback);
 
         void draw(sf::RenderWindow &window);
         void update(sf::RenderWindow &window);
@@ -37,16 +39,12 @@ class TextButton {
         sf::Vector2f m_buttonSize = sf::Vector2f(0, 0);
         sf::Color m_buttonColor = sf::Color::White;
         sf::Color m_buttonOutlineColor = sf::Color::White;
-        int m_buttonOutlineThickness = 0;
         sf::Color m_buttonHoverColor = sf::Color::White;
         sf::Color m_buttonHoverOutlineColor = sf::Color::White;
 
         sf::Text m_text = sf::Text();
-        std::string m_textString = "";
-        sf::Font m_textFont = sf::Font();
-        int m_textSize = 0;
         sf::Color m_textColor = sf::Color::White;
         sf::Color m_textHoverColor = sf::Color::White;
 
-        void (*m_callback)() = nullptr;
+        std::function<void()> m_callback = [](){};
 };
