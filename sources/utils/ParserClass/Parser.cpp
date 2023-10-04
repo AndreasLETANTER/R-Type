bad9dfd11daee65484444190789a4900a0b64383
Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
2023-10-04 10:10:30 +0200

    feat(ConfigParser): add loadObstacle method implementation

--- sources/utils/ParserClass/Parser.cpp
+++ sources/utils/ParserClass/Parser.cpp
@@ -50,13 +50,40 @@ void Parser::loadPlayers()
 
     for (int i = 0; players.getLength(); i++) {
         std::string assetName = players[i]["assetName"];
+        std::string projectileAssetName = players[i]["projectileAssetName"];
         Component::Position position = Component::Position(players[i]["position"]["x"], players[i]["position"]["y"]);
         Component::Position scale = Component::Position(players[i]["scale"]["x"], players[i]["scale"]["y"]);
         sf::IntRect rect = sf::IntRect(players[i]["rect"]["x"], players[i]["rect"]["y"], players[i]["rect"]["width"], players[i]["rect"]["height"]);
+        int health = players[i]["health"];
 
         std::cout << "assetName: " << assetName << std::endl;
+        std::cout << "projectileAssetName: " << projectileAssetName << std::endl;
         std::cout << "position: " << position.x << ", " << position.y << std::endl;
         std::cout << "scale: " << scale.x << ", " << scale.y << std::endl;
         std::cout << "rect: " << rect.left << ", " << rect.top << ", " << rect.width << ", " << rect.height << std::endl;
+        std::cout << "health: " << health << std::endl;
+    }
+}
+
+void Parser::loadObstacles()
+{
+    const libconfig::Setting &obstacles = m_config.lookup("obstacles");
+
+    for (int i = 0; obstacles.getLength(); i++) {
+        std::string assetName = obstacles[i]["assetName"];
+        std::string projectileAssetName = players[i]["projectileAssetName"];
+        Component::Position position = Component::Position(obstacles[i]["position"]["x"], obstacles[i]["position"]["y"]);
+        Component::Position scale = Component::Position(obstacles[i]["scale"]["x"], obstacles[i]["scale"]["y"]);
+        sf::IntRect rect = sf::IntRect(obstacles[i]["rect"]["x"], obstacles[i]["rect"]["y"], obstacles[i]["rect"]["width"], obstacles[i]["rect"]["height"]);
+        int rotation = obstacles[i]["rotation"];
+        int health = obstacles[i]["health"];
+
+        std::cout << "assetName: " << assetName << std::endl;
+        std::cout << "projectileAssetName: " << projectileAssetName << std::endl;
+        std::cout << "position: " << position.x << ", " << position.y << std::endl;
+        std::cout << "scale: " << scale.x << ", " << scale.y << std::endl;
+        std::cout << "rect: " << rect.left << ", " << rect.top << ", " << rect.width << ", " << rect.height << std::endl;
+        std::cout << "rotation: " << rotation << std::endl;
+        std::cout << "health: " << health << std::endl;
     }
 }
