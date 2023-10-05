8cd6e030e6ae96c404a5c85a464bd4a6186cc7da
Eliot Baratte <eliot.baratte@epitech.eu>
2023-10-05 13:54:17 +0200

    docs(MainMenu): add doxygen documentation

--- sources/client/MainMenu/MainMenu.hpp
+++ sources/client/MainMenu/MainMenu.hpp
@@ -11,12 +11,20 @@
 
 #include "client/Buttons/TextButton/TextButton.hpp"
 
+/// @brief Class to create the main menu with Play and Quit buttons
 class MainMenu {
     public:
+        /// @brief Constructor of MainMenu which display a scrolling background and two buttons (Play and Quit)
+        /// @param window The window where the main menu will be displayed
         MainMenu(sf::RenderWindow &window);
 
+        /// @brief Draw the buttons on the window
         void draw();
+
+        /// @brief Update the buttons to check if they are hovered or clicked
         void update();
+
+        /// @brief Resize the buttons to fit the new window size
         void resize();
 
     private:
