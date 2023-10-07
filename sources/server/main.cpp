fe6deab88003283f883d9c94743b13f1f793a5d8
Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
2023-10-07 15:37:39 +0200

    refactor: main to not use projectile collision and health components

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -17,9 +17,7 @@
 #include "ECS/Systems/AutoMoveSystem/AutoMoveSystem.hpp"
 #include "ECS/Systems/ShootSystem/ShootSystem.hpp"
 #include "ECS/Systems/ProjectileSystem/ProjectileSystem.hpp"
-#include "ECS/Systems/ProjectileCollisionSystem/ProjectileCollisionSystem.hpp"
 #include "ECS/Systems/CollisionSystem/CollisionSystem.hpp"
-#include "ECS/Systems/HealthSystem/HealthSystem.hpp"
 #include "ECS/RegistryClass/Registry.hpp"
 #include "ECS/Systems/ScrollingBackgroundSystem/ScrollingBackgroundSystem.hpp"
 
@@ -33,11 +31,6 @@ int main(const int ac, const char **av)
     std::vector<std::string> filesPath = {"./assets/Level1.yaml"};
     Parser parser(registry, window, clock, filesPath);
 
-    registry.spawn_entity();
-    registry.spawn_entity();
-    registry.spawn_entity();
-    registry.spawn_entity();
-
     registry.register_component<Component::Position>();
     registry.register_component<Component::Velocity>();
     registry.register_component<Component::Controllable>();
@@ -46,34 +39,7 @@ int main(const int ac, const char **av)
     registry.register_component<Component::Shoot>();
     registry.register_component<Component::Projectile>();
     registry.register_component<Component::Collision>();
-    registry.register_component<Component::Health>();
     registry.register_component<Component::ScrollingBackground>();
-
-    // Background
-    registry.add_component<Component::Position>(registry.entity_from_index(0), Component::Position(0, 0));
-    registry.add_component<Component::Drawable>(registry.entity_from_index(0), Component::Drawable("Space_Background.png", &window, sf::IntRect(0, 0, 300, 207), Component::Position(0, 0), true));
-    registry.add_component<Component::ScrollingBackground>(registry.entity_from_index(0), Component::ScrollingBackground(Component::Position(0, 0), Component::Position(-5700, 0)));
-    registry.add_component<Component::Position>(registry.entity_from_index(1), Component::Position(5700, 0));
-    registry.add_component<Component::Drawable>(registry.entity_from_index(1), Component::Drawable("Space_Background.png", &window, sf::IntRect(0, 0, 300, 207), Component::Position(0, 0), true));
-    registry.add_component<Component::ScrollingBackground>(registry.entity_from_index(1), Component::ScrollingBackground(Component::Position(5700, 0), Component::Position(0, 0)));
-
-    //entity that is movable, using all components.
-    registry.add_component<Component::Position>(registry.entity_from_index(2), Component::Position(0, 0));
-    registry.add_component<Component::Velocity>(registry.entity_from_index(2), Component::Velocity(0, 0));
-    registry.add_component<Component::Controllable>(registry.entity_from_index(2), Component::Controllable(true));
-    registry.add_component<Component::Drawable>(registry.entity_from_index(2), Component::Drawable("NugoTemporaryIcon.png", &window, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), true));
-    registry.add_component<Component::Shoot>(registry.entity_from_index(2), Component::Shoot(true, &clock, sf::Time(sf::milliseconds(250)), 20, "BurpTemporaryBullet.png", false));
-    registry.add_component<Component::Collision>(registry.entity_from_index(2), Component::Collision(80, 80));
-    registry.add_component<Component::Health>(registry.entity_from_index(2), Component::Health(100));
-
-    //monster that automove from top to bottom
-    registry.add_component<Component::Position>(registry.entity_from_index(3), Component::Position(1000, 0));
-    registry.add_component<Component::Velocity>(registry.entity_from_index(3), Component::Velocity(0, 0));
-    registry.add_component<Component::Drawable>(registry.entity_from_index(3), Component::Drawable("NugoTemporaryIcon.png", &window, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), true));
-    registry.add_component<Component::AutoMove>(registry.entity_from_index(3), Component::AutoMove(Component::Position(1000, 0), Component::Position(1000, 500)));
-    registry.add_component<Component::Shoot>(registry.entity_from_index(3), Component::Shoot(true, &clock, sf::Time(sf::milliseconds(250)), 20, "BurpTemporaryBullet.png", true));
-    registry.add_component<Component::Collision>(registry.entity_from_index(3), Component::Collision(80, 80));
-    registry.add_component<Component::Health>(registry.entity_from_index(3), Component::Health(100));
 
     registry.add_system<Component::Position, Component::Velocity>(PositionSystem());
     registry.add_system<Component::Controllable, Component::Velocity>(ControlSystem());
@@ -81,11 +47,9 @@ int main(const int ac, const char **av)
     registry.add_system<Component::Position, Component::AutoMove>(AutoMoveSystem());
     registry.add_system<Component::Shoot, Component::Position, Component::Drawable>(ShootSystem());
     registry.add_system<Component::Projectile, Component::Position, Component::Velocity>(ProjectileSystem());
-    registry.add_system<Component::Projectile, Component::Collision, Component::Health>(ProjectileCollisionSystem());
     registry.add_system<Component::Position, Component::Collision>(CollisionSystem());
     registry.add_system<Component::Position, Component::ScrollingBackground>(ScrollingBackgroundSystem());
-    registry.add_system<Component::Health>(HealthSystem());
-  
+
     parser.loadFromFile();
     window.setFramerateLimit(144);
 
