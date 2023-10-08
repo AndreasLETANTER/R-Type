77c878c8554500abe9ba489b751e1ffc9a31fae1
hugon <hugo.noury@epitech.eu>
2023-10-08 15:14:13 +0200

    feat: add tcp client part with signals and basic errors handling

--- sources/client/main.cpp
+++ sources/client/main.cpp
@@ -6,22 +6,44 @@
 */
 
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
 
 #include "ECS/RegistryClass/Registry.hpp"
 #include "ECS/Systems/PositionSystem/PositionSystem.hpp"
 #include "ECS/Systems/DrawSystem/DrawSystem.hpp"
 #include "ECS/Systems/ScrollSystem/ScrollSystem.hpp"
 #include "client/MainMenu/MainMenu.hpp"
+#include "client/Network/Network.hpp"
+
+void signalHandler(int signum)
+{
+    std::cout << "Interrupt signal (" << signum << ") received.\n";
+    exit(signum);
+}
 
 int main(int ac, char **av)
 {
-    (void) ac;
-    (void) av;
+    Network network = Network(strtoul(av[1], nullptr, 10), av[2]);
+    std::string messageFromServer;
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Hess-Type");
     Registry registry;
     window.setFramerateLimit(144);
     sf::Clock clock;
     MainMenu mainMenu(window);
+
+    if (ac != 3) {
+        std::cerr << "Usage: ./r-type_client <port> <ip_address>" << std::endl;
+        return (84);
+    }
+    network.launchConnection();
+    signal(SIGINT, signalHandler);
+    network.sendMessageToServer("Hello World");
+    while (!network.getCommunicationEnded()) {
+        messageFromServer = network.receiveMessageFromServer();
+        if (!messageFromServer.empty())
+            std::cout << "received: " << messageFromServer << std::endl;
+    }
+    network.closeConnectedSocket();
 
     registry.spawn_entity();
     registry.spawn_entity();
