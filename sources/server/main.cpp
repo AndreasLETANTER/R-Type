4d428ee0a7d71d5b5063d0005b71af46b23750da
Luchooooo <louis.barreteau@epitech.eu>
2023-10-11 17:28:23 +0200

    test: registry

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -76,8 +76,6 @@ int main(const int ac, const char **av)
         received = udpServer.receive();
         if (received != nullptr) {
             t_input input = converter.convertBinaryToInput(received);
-            std::cout << "Input received id: " << input.id << std::endl;
-            std::cout << "Input received key: " << input.key << std::endl;
         }
         std::pair<message_t *, size_t> messages = registry.exportToMessages();
         udpServer.send(converter.convertStructToBinary(messages.second, messages.first));
