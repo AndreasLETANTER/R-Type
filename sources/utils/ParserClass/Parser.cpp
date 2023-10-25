78632831146d7186f638b298e0bef6edf3cf0156
Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
2023-10-25 14:20:52 +0200

    refactor: adds shootingDelay in configuration file and parser

--- sources/utils/ParserClass/Parser.cpp
+++ sources/utils/ParserClass/Parser.cpp
@@ -107,12 +107,13 @@ void Parser::loadPlayers()
         base.rectHeight = players[i]["rect"]["height"];
         int health = players[i]["health"];
         int damage = players[i]["damage"];
+        int shootingDelay = players[i]["shootingDelay"];
 
         m_registry.spawn_entity();
         m_registry.add_component<Component::Position>(m_registry.entity_from_index(base.id), Component::Position(base.posX, base.posY));
         m_registry.add_component<Component::Velocity>(m_registry.entity_from_index(base.id), Component::Velocity(0, 0));
         m_registry.add_component<Component::Controllable>(m_registry.entity_from_index(base.id), Component::Controllable(true, i + 1));
-        m_registry.add_component<Component::Shoot>(m_registry.entity_from_index(base.id), Component::Shoot(i + 1, true, LINEAR, &m_clock, sf::Time(sf::milliseconds(500)), damage, projectileAssetName, 1, 1920));
+        m_registry.add_component<Component::Shoot>(m_registry.entity_from_index(base.id), Component::Shoot(i + 1, true, LINEAR, &m_clock, sf::Time(sf::milliseconds(shootingDelay)), damage, projectileAssetName, 1, 1920));
         m_registry.add_component<Component::Drawable>(m_registry.entity_from_index(base.id), Component::Drawable(base.assetName, &m_window, sf::IntRect(base.rectX, base.rectY, base.rectWidth, base.rectHeight), Component::Position(base.scaleX, base.scaleY), m_registry.get_assets().get_texture(base.assetName)));
         m_registry.add_component<Component::Collision>(m_registry.entity_from_index(base.id), Component::Collision(base.rectHeight, base.rectWidth));
         m_registry.add_component<Component::Health>(m_registry.entity_from_index(base.id), Component::Health(health));
@@ -173,6 +174,7 @@ void Parser::loadEnemies()
         int rotation = enemies[i]["rotation"];
         int health = enemies[i]["health"];
         int damage = enemies[i]["damage"];
+        int shootingDelay = enemies[i]["shootingDelay"];
         int pattern = enemies[i]["shootingPattern"];
         ShootingPattern sPattern = static_cast<ShootingPattern>(pattern);
         (void) rotation;
@@ -180,7 +182,7 @@ void Parser::loadEnemies()
         m_registry.spawn_entity();
         m_registry.add_component<Component::Position>(m_registry.entity_from_index(base.id), Component::Position(base.posX, base.posY));
         m_registry.add_component<Component::Velocity>(m_registry.entity_from_index(base.id), Component::Velocity(0, 0));
-        m_registry.add_component<Component::Shoot>(m_registry.entity_from_index(base.id), Component::Shoot(0, true, sPattern, &m_clock, sf::Time(sf::milliseconds(3000)), damage, projectileAssetName, -1, 0));
+        m_registry.add_component<Component::Shoot>(m_registry.entity_from_index(base.id), Component::Shoot(0, true, sPattern, &m_clock, sf::Time(sf::milliseconds(shootingDelay)), damage, projectileAssetName, -1, 0));
         m_registry.add_component<Component::Drawable>(m_registry.entity_from_index(base.id), Component::Drawable(base.assetName, &m_window, sf::IntRect(base.rectX, base.rectY, base.rectWidth, base.rectHeight), Component::Position(base.scaleX, base.scaleY), m_registry.get_assets().get_texture(base.assetName)));
         m_registry.add_component<Component::Collision>(m_registry.entity_from_index(base.id), Component::Collision(base.rectHeight, base.rectWidth));
         m_registry.add_component<Component::AutoMove>(m_registry.entity_from_index(base.id), Component::AutoMove(Component::Position(base.posX, base.posY), Component::Position(automove_x, automove_y)));
