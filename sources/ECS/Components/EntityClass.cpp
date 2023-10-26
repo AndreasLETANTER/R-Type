cd2164924b1da042198ba7b15c2728f033a3acc7
Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
2023-10-26 22:23:01 +0200

    feat: adds some sort of differences between the classes available

--- sources/ECS/Components/EntityClass.cpp
+++ sources/ECS/Components/EntityClass.cpp
@@ -22,17 +22,17 @@ namespace Component {
     EntityClass EntityClassFactory::CreateEntityClass(EntityClasses entityClass) {
         switch (entityClass) {
             case EntityClasses::NUGO:
-                return EntityClass("SpaceShips.gif", "BulletSprite.png", Component::Position(102, 57), sf::IntRect(0, 2, 34, 16), 100, 5, 500, 5, ShootingPattern::LINEAR);
+                return EntityClass("SpaceShips.gif", "BulletSprite.png", Component::Position(102, 57), sf::IntRect(0, 2, 34, 16), 150, 15, 750, 1, ShootingPattern::LINEAR);
             case EntityClasses::ANDREAS:
-                return EntityClass("SpaceShips.gif", "BulletSprite.png", Component::Position(102, 57), sf::IntRect(0, 19, 34, 16), 100, 5, 500, 5, ShootingPattern::LINEAR);
+                return EntityClass("SpaceShips.gif", "BulletSprite.png", Component::Position(102, 57), sf::IntRect(0, 19, 34, 16), 50, 1, 300, 7, ShootingPattern::LINEAR);
             case EntityClasses::ELIOT:
                 return EntityClass("SpaceShips.gif", "BulletSprite.png", Component::Position(102, 57), sf::IntRect(0, 36, 34, 16), 100, 5, 500, 5, ShootingPattern::LINEAR);
             case EntityClasses::LOUIS:
-                return EntityClass("SpaceShips.gif", "BulletSprite.png", Component::Position(102, 57), sf::IntRect(0, 53, 34, 16), 100, 5, 500, 5, ShootingPattern::LINEAR);
+                return EntityClass("SpaceShips.gif", "BulletSprite.png", Component::Position(102, 57), sf::IntRect(0, 53, 34, 16), 75, 5, 500, 3, ShootingPattern::LINEAR);
             case EntityClasses::MOB_ORANGE_CRAB:
-                return EntityClass("OrangeCrabEnemy.gif", "BulletSpriteMonster.png", Component::Position(99, 87), sf::IntRect(0, 1, 33, 29), 10, 25, 3000, 5, ShootingPattern::SPREAD);
+                return EntityClass("OrangeCrabEnemy.gif", "BulletSpriteMonster.png", Component::Position(99, 87), sf::IntRect(0, 1, 33, 29), 30, 5, 2000, 5, ShootingPattern::SPREAD);
             case EntityClasses::MOB_YELLOW_POPCORN:
-                return EntityClass("YellowPopcornEnemy.gif", "BulletSpriteMonster.png", Component::Position(99, 87), sf::IntRect(0, 1, 33, 29), 10, 25, 3000, 5, ShootingPattern::CIRCULAR);
+                return EntityClass("YellowPopcornEnemy.gif", "BulletSpriteMonster.png", Component::Position(99, 87), sf::IntRect(0, 1, 33, 29), 10, 3, 1000, 7, ShootingPattern::CIRCULAR);
             default:
                 return EntityClass("SpaceShips.gif", "BulletSprite.png", Component::Position(102, 57), sf::IntRect(0, 2, 34, 16), 100, 5, 500, 5, ShootingPattern::LINEAR);
         }
