9fe407f39e92c923189ebb0d379076760855862f
Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
2023-10-26 23:05:03 +0200

    test: fixes compilation problems due to new functionnalities

--- tests/ECS/RegistryExportTests.cpp
+++ tests/ECS/RegistryExportTests.cpp
@@ -24,12 +24,12 @@ Test(Registry, basic_exportToPackets)
 
     auto entity1 = reg.spawn_entity();
     reg.add_component<Component::Position>(entity1, Component::Position(0, 0));
-    reg.add_component<Component::Velocity>(entity1, Component::Velocity(0, 0));
-    reg.add_component<Component::Drawable>(entity1, Component::Drawable("BulletSprite.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("BulletSprite.png")));
+    reg.add_component<Component::Velocity>(entity1, Component::Velocity(0, 0, 0));
+    reg.add_component<Component::Drawable>(entity1, Component::Drawable("NugoBullet.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("NugoBullet.png")));
 
     std::vector<packet_t> packets = reg.exportToPackets();
 
-    cr_assert_eq(strcmp(packets[0].message.sprite_name, "BulletSprite.png"), 0);
+    cr_assert_eq(strcmp(packets[0].message.sprite_name, "NugoBullet.png"), 0);
     cr_assert_eq(packets[0].message.x, 0);
     cr_assert_eq(packets[0].message.y, 0);
     cr_assert_eq(packets.size(), 2);
@@ -46,18 +46,18 @@ Test(Registry, 2_spawn_exportToPackets)
     auto entity1 = reg.spawn_entity();
     auto entity2 = reg.spawn_entity();
     reg.add_component<Component::Position>(entity1, Component::Position(0, 0));
-    reg.add_component<Component::Velocity>(entity1, Component::Velocity(0, 0));
-    reg.add_component<Component::Drawable>(entity1, Component::Drawable("BulletSprite.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("BulletSprite.png")));
+    reg.add_component<Component::Velocity>(entity1, Component::Velocity(0, 0, 0));
+    reg.add_component<Component::Drawable>(entity1, Component::Drawable("NugoBullet.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("NugoBullet.png")));
     reg.add_component<Component::Position>(entity2, Component::Position(3, 5));
-    reg.add_component<Component::Velocity>(entity2, Component::Velocity(0, 0));
-    reg.add_component<Component::Drawable>(entity2, Component::Drawable("BulletSprite.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("BulletSprite.png")));
+    reg.add_component<Component::Velocity>(entity2, Component::Velocity(0, 0, 0));
+    reg.add_component<Component::Drawable>(entity2, Component::Drawable("NugoBullet.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("NugoBullet.png")));
 
     std::vector<packet_t> packets = reg.exportToPackets();
 
-    cr_assert_eq(strcmp(packets[0].message.sprite_name, "BulletSprite.png"), 0);
+    cr_assert_eq(strcmp(packets[0].message.sprite_name, "NugoBullet.png"), 0);
     cr_assert_eq(packets[0].message.x, 0);
     cr_assert_eq(packets[0].message.y, 0);
-    cr_assert_eq(strcmp(packets[1].message.sprite_name, "BulletSprite.png"), 0);
+    cr_assert_eq(strcmp(packets[1].message.sprite_name, "NugoBullet.png"), 0);
     cr_assert_eq(packets[1].message.x, 3);
     cr_assert_eq(packets[1].message.y, 5);
     cr_assert_eq(packets.size(), 4);
@@ -74,14 +74,14 @@ Test(Registry, 50_spawn_exportToPackets)
     for (int i = 0; i < 50; i++) {
         auto entity = reg.spawn_entity();
         reg.add_component<Component::Position>(entity, Component::Position(i, i));
-        reg.add_component<Component::Velocity>(entity, Component::Velocity(0, 0));
-        reg.add_component<Component::Drawable>(entity, Component::Drawable("BulletSprite.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("BulletSprite.png")));
+        reg.add_component<Component::Velocity>(entity, Component::Velocity(0, 0, 0));
+        reg.add_component<Component::Drawable>(entity, Component::Drawable("NugoBullet.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("NugoBullet.png")));
     }
 
     std::vector<packet_t> packets = reg.exportToPackets();
 
     for (int i = 0; i < 50; i++) {
-        cr_assert_eq(strcmp(packets[i].message.sprite_name, "BulletSprite.png"), 0);
+        cr_assert_eq(strcmp(packets[i].message.sprite_name, "NugoBullet.png"), 0);
         cr_assert_eq(packets[i].message.x, i);
         cr_assert_eq(packets[i].message.y, i);
     }
@@ -99,14 +99,14 @@ Test(Registry, 800_spawn_exportToPackets)
     for (int i = 0; i < 800; i++) {
         auto entity = reg.spawn_entity();
         reg.add_component<Component::Position>(entity, Component::Position(i, i));
-        reg.add_component<Component::Velocity>(entity, Component::Velocity(0, 0));
-        reg.add_component<Component::Drawable>(entity, Component::Drawable("BulletSprite.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("BulletSprite.png")));
+        reg.add_component<Component::Velocity>(entity, Component::Velocity(0, 0, 0));
+        reg.add_component<Component::Drawable>(entity, Component::Drawable("NugoBullet.png", nullptr, sf::IntRect(0, 0, 0, 0), Component::Position(0, 0), reg.get_assets().get_texture("NugoBullet.png")));
     }
 
     std::vector<packet_t> packets = reg.exportToPackets();
 
     for (int i = 0; i < 800; i++) {
-        cr_assert_eq(strcmp(packets[i].message.sprite_name, "BulletSprite.png"), 0);
+        cr_assert_eq(strcmp(packets[i].message.sprite_name, "NugoBullet.png"), 0);
         cr_assert_eq(packets[i].message.x, i);
         cr_assert_eq(packets[i].message.y, i);
     }
