5c8598a5056c921c27cc37fe5e157e8c38ee8d04
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-11-01 20:40:38 +0100

    feat: setup of the scrolling boss

--- sources/ECS/Systems/WaveSystem/WaveSystem.cpp
+++ sources/ECS/Systems/WaveSystem/WaveSystem.cpp
@@ -20,19 +20,19 @@ WaveSystem WaveSystem::operator()(Registry &registry)
     if (registry.enemiesAreDead()) {
         if (current_wave_index == 1) {
             unsigned int enemy_type = rand() % m_boss_vector.size();
-            unsigned int x_offset = rand() % (2000 - 1920) + 1 + 1920 + (0 * 100);
-            unsigned int x_start = rand() % (1000 - 400) + 1 + 400;
-            unsigned int y_start = rand() % 1000;
+            Component::EntityClass entityClassTmp = m_boss_vector[enemy_type];
+            unsigned int x_offset = rand() % (2000 - 1920) + 1 + 1920;
+            unsigned int x_start = 1800 - entityClassTmp.scale.x;
+            unsigned int y_start = 1080 / 2 - entityClassTmp.scale.y / 2;
             auto enemy = registry.spawn_entity();
 
-            Component::EntityClass entityClassTmp = m_boss_vector[enemy_type];
             registry.add_component<Component::Group>(enemy, Component::Group(2));
             registry.add_component<Component::Position>(enemy, Component::Position(x_offset, y_start));
             registry.add_component<Component::Velocity>(enemy, Component::Velocity(0, 0, entityClassTmp.speed));
             registry.add_component<Component::Shoot>(enemy, Component::Shoot(0, true, entityClassTmp.shootingPattern, registry.getClock(), sf::Time(sf::milliseconds(entityClassTmp.shootingDelay)), entityClassTmp.damage, entityClassTmp.projectileAssetName, -1, 0));
             registry.add_component<Component::Drawable>(enemy, Component::Drawable(entityClassTmp.assetName, registry.getWindow(), sf::IntRect(entityClassTmp.rect.left, entityClassTmp.rect.top, entityClassTmp.rect.width, entityClassTmp.rect.height), Component::Position(entityClassTmp.scale.x, entityClassTmp.scale.y), registry.get_assets().get_texture(entityClassTmp.assetName)));
             registry.add_component<Component::Collision>(enemy, Component::Collision(entityClassTmp.scale.y, entityClassTmp.scale.x));
-            registry.add_component<Component::AutoMove>(enemy, Component::AutoMove(Component::Position(x_start, y_start), Component::Position(x_start - 300, y_start)));
+            registry.add_component<Component::Scroll>(enemy, Component::Scroll(Component::Position(x_offset, y_start), Component::Position(0, 0), x_start));
             registry.add_component<Component::Health>(enemy, Component::Health(entityClassTmp.health));
         } else {
             for (unsigned int i = 0; i < nb_enemies_for_wave; i++) {
