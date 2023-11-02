9c5f867cbc6d29a7dbec035663f3b8acc71331f9
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-11-02 17:22:55 +0100

    feat: boss now shoot projectiles from top to bottom then bottom to top

--- sources/ECS/Systems/ShootSystem/ShootingPatterns/RandomShootingPattern.cpp
+++ sources/ECS/Systems/ShootSystem/ShootingPatterns/RandomShootingPattern.cpp
@@ -21,20 +21,27 @@ void RandomShootingPattern::shoot(Registry &registry, Component::Shoot &shoot, C
         double ringRadius = 50.0; // Radius of the outermost ring
         double ringSpacing = 30.0; // Spacing between rings
 
-        for (int i = 0; i < numRings; ++i) {
-            double ringAngleStep = 360.0 / projectilesInRing;
-            for (int j = 0; j < projectilesInRing; ++j) {
-                double angle = j * ringAngleStep + (i * 10); // Add an offset for spiral effect
-                double radians = angle * (3.14159265359 / 180.0);
-                double ringRadiusOffset = i * ringSpacing;
+        static int y_direction = 0;
+        static int y_direction_offset = 400;
 
+        if (y_direction >= 1920 || y_direction < 0) {
+            y_direction_offset = -y_direction_offset;
+        }
+
+        for (int i = 0; i < numRings; i++) {
+            double ringAngleStep = 360.0 / projectilesInRing;
+            for (int j = 0; j < projectilesInRing; j++) {
+                double angle = j * ringAngleStep + (i * 10); // Add an offset for spiral effect
+                double radians = angle * (3.14 / 180.0);
+                double ringRadiusOffset = i * ringSpacing;
                 double offsetX = cos(radians) * (ringRadius + ringRadiusOffset);
                 double offsetY = sin(radians) * (ringRadius + ringRadiusOffset);
 
-                createProjectile(registry, window, shoot.bulletSpriteName, Component::Position(x + offsetX, y + offsetY), Component::Position(0, y + offsetY), shoot.damage, shoot.playerId, groupId);
+                createProjectile(registry, window, shoot.bulletSpriteName, Component::Position(x + offsetX, y + offsetY), Component::Position(0, y_direction + offsetY), shoot.damage, shoot.playerId, groupId);
             }
         }
-
+        std::cout << y_direction << std::endl;
+        y_direction += y_direction_offset;
         shoot.lastShot = shoot.clock->getElapsedTime();
     }
 }
