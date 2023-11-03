dbfdb82ce7426c0f3e957d8255543cfe2a5bd61c
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-11-03 03:13:45 +0100

    fix: Add new macro usefull

--- sources/ECS/RegistryClass/Registry.hpp
+++ sources/ECS/RegistryClass/Registry.hpp
@@ -28,6 +28,8 @@
 #define ENTITY_MOVE_CODE 104
 #define ENTITY_SCORE_CODE 105
 
+#define CLIENT_INPUT_CODE 200
+
 /**
  * @brief Struct representing a message containing the sprite name position and score of an entity.
  */
