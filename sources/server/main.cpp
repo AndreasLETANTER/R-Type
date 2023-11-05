e6b429673db9d2634aee85a486de303348eb4d40
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-11-05 22:37:30 +0100

    fix: check for number of argumetns before ru ning game mode

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -15,6 +15,11 @@ int main(const int ac, const char **av)
 {
     GameModeFactory factory;
     std::unique_ptr<IGameMode> gameMode;
+
+    if (ac < 5) {
+        std::cerr << "Usage: ./r-type_server <port> <map> <gameMode> [args]" << std::endl;
+        return 84;
+    }
     try {
         gameMode = factory.createGameMode(av[4], av, ac, true);
         gameMode->init();
